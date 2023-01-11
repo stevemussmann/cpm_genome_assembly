b909c34617f475d0406569549cc87c0452c46988
Steve Mussmann <17727337+stevemussmann@users.noreply.github.com>
2023-01-11 13:47:06 -0800

    Add files via upload

--- 06_contaminant_filtering/02_removeContamination.sh
+++ 06_contaminant_filtering/02_removeContamination.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-#9606 is human - ignoring those pulls out just fungus and bacteria matches
-grep -v 9606 pluc_classified.txt | grep -A 1 ">" > pluc_hic_genome.3ddna.kraken2.removed.fa
+CLASSIFIED="pluc_classified.fa"
+UNCLASSIFIED="pluc_unclassified.fa"
 
+REMOVE="pluc_hic_genome.3ddna.kraken2.removed.fa"
+NOCONTAM="pluc_hic_genome.3ddna.nocontamination.fa"
+
+#9606 is human - ignoring those pulls out just fungus and bacteria matches
+grep -v 9606 $CLASSIFIED | grep -A 1 ">" > $REMOVE
+
+# remove the lines with dashes that were added by the above code for some reason
+grep -v -- "-" $REMOVE > tmp
+mv tmp $REMOVE
+
+# cat the unclassified scaffolds back to the classified fasta
+cat $UNCLASSIFIED >> $CLASSIFIED
+
+./matchHeader.pl -c $CLASSIFIED -r $REMOVE -o $NOCONTAM
 
 
 exit 
